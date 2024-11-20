bad7b4e1360bc27306b2830d5a82b14fb19a7bf5
Paramesha33 <94032830+Paramesha33@users.noreply.github.com>
2024-11-20 14:52:06 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+char size(int cms)
+{
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms < 38) 
+    {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms < 42) 
+    {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms > 42)
+    {
         sizeName = 'L';
     }
     return sizeName;
@@ -17,6 +21,9 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+    assert(size(38) == 'S');
+    assert(size(42) == 'M');
+    assert(size(-1) == 'S');
     printf("All is well (maybe!)\n");
     return 0;
 }
